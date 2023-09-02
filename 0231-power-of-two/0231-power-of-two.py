8303b46205527a0b86676c373be736c06feb67bd
Wanjiku Kangangi <wanjikukangangi@gmail.com>
2023-09-02 13:27:38 +0300

    Time: 39 ms (78.42%), Space: 16.3 MB (60.34%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,4 +1,7 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        return n > 0 and (n & (n - 1)) == 0
+        if n <= 0:
+            return False
+        log_base_2 = math.log2(n)
+        return log_base_2.is_integer()
         
