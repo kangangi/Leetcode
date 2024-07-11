671c1c016bf41503af1880fdc38dc462388ba8dd
Wanjiku Kangangi <wanjikukangangi@gmail.com>
2024-07-11 11:23:11 +0300

    Time: 174 ms (41.44%), Space: 18 MB (14.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        from collections import defaultdict
-        data = defaultdict(int)
-        threshold = len(nums) / 2
-        for i in nums:
-            data[i] += 1
-            if data[i] > threshold:
-                return i
+        k, count = 0, 0
+        
+        for num in nums:
+            if count == 0:
+                k = num
+            count = count + 1 if num == k else count -1
+        return k
         
