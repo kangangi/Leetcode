726734dea35ab96d6ed8458a7877de47399db161
Wanjiku Kangangi <wanjikukangangi@gmail.com>
2024-08-07 16:04:56 +0300

    Time: 65 ms (17.64%), Space: 17.9 MB (11.08%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,7 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
+        if not prices:
+            return 0
         
-        for i in range(1, len(prices)):
-            profit += max(0, prices[i]- prices[i-1])
-        return profit
+        no_stock = 0
+        has_stock = -prices[0]
+        
+        for price in prices[1:]:
+            new_no_stock = max(no_stock, has_stock + price)
+            new_has_stock = max(has_stock, no_stock - price)
+            no_stock, has_stock = new_no_stock, new_has_stock
+        
+        return no_stock
